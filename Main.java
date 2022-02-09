e113509bf85d3339236ed29c8cfe8ed4bb45ea8f
Santhosh13112002 <santhoshsaravananmj@gmail.com>
2022-02-09 11:11:07 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class A
+{
+      public A()
+      {
+            System.out.println("First Git");
+      }
+
+}
+
+public class Main
+{
+      public static void main(String args[])
+      {
+            A obj=new A();
+      }
+}
