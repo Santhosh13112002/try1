500f776a78211f9cffa6aa70dac7209929383a7c
Santhosh13112002 <santhoshsaravananmj@gmail.com>
2022-02-09 12:05:26 +0530

    commmit 3

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class A
 {
       public A()
       {
-            a=10;
-            b=20;
+            int a=10;
+            int b=20;
             System.out.println("welcome to java programming");
             System.out.println(a);
             System.out.println(b);
